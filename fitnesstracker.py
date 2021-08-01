7d394a8e9c46b95d687eb0b8b9cf7cb275b711c2
FerencK <kissf@starschema.net>
2021-08-01 16:59:03 +0200

    erd + minor changes

--- fitnesstracker.py
+++ fitnesstracker.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from datetime import datetime
+from test import URL_BASE
 from stravalib.client import Client
 import os
 import time as t
@@ -9,8 +10,9 @@ import traceback
 CLIENT_ID = os.environ.get('CLIENT_ID')
 CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 REFRESH_TOKEN = os.environ.get('REFRESH_TOKEN')
-"""ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')"""
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 EXPIRES_AT = os.environ.get('EXPIRES_AT')
+URL_BASE = 'https://www.strava.com/api/v3/'
 REDIRECT_URL = 'localhost'
 ATHLETE_ID = '44086627'
 
@@ -28,18 +30,24 @@ def token_refresh():
     resp = client.refresh_access_token(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,refresh_token=REFRESH_TOKEN)
     os.environ['ACCESS_TOKEN'] = resp['access_token']
     os.environ['EXPIRES_AT'] = str(resp['expires_at'])
-    """SCRIPT HERE TO PERMANENTLY REPLACE THE VARIABLES"""
+    with open('dat', 'w') as dat:
+        for key in resp:
+            if key in ['access_token','expires_at']:
+                dat.write(key+' '+str(resp[key])+'\n')
+    dat.close
+    os.system('token_replace.bat')
     
 
 def get_activities():
     """The aim for this function is to check the last activity in the DBd and fetch any new after that.
     For now this will be a static 7 day fetch."""
-    client = Client()
-    aftr = datetime.datetime.today() - timedelta(days=7)
+    client = Client(access_token=ACCESS_TOKEN)
+    aftr = datetime.today() - timedelta(days=7)
     acts = client.get_activities(after=aftr)
     actList = []
     for i in acts:
         actList.append(i.id)
+    return actList
 
 """resp = requests.get('https://www.strava.com/api/v3/activities/5675641194',headers={'Authorization':'Bearer ' + ACCESS_TOKEN})"""
 
@@ -50,6 +58,10 @@ print(datetime.datetime.fromtimestamp(resp['expires_at']).strftime('%c'))
 
 print (resp.json())
 
+BASE/athlete
+BASE/athlete/zones
+BASE/activities/ID/streams?keys=heartrate
+BASE/activities/ID/streams
 
 
 http://www.strava.com/oauth/authorize?client_id=47499&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=profile:read_all,activity:read_all
@@ -59,20 +71,15 @@ http://www.strava.com/oauth/authorize?client_id=47499&response_type=code&redirec
 if __name__ == '__main__':
     
     try:
-        
         exp = exp_checker(EXPIRES_AT)
         if exp == 'ok':
-            "CODE COMES HERE"
             print('OK')
+            print(get_activities())
+
         else:
-            print('As the token will expire soon, it will be refreshed.')
-            print('.')
-            t.sleep(1)
             token_refresh()
-            t.sleep(1)
-            print('.')
-            print('New token has been generated and credentials are refreshed.')
-    
+            print(get_activities())
+            
     except Exception:
         print(traceback.format_exception(BaseException,BaseException,None))
         print(datetime.now())
